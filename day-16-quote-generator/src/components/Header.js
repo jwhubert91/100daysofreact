e97b2b0b91b50fe083bf43b41c8982b49bfb7b6c
james w hubert <jwhubert91@gmail.com>
2020-11-25 21:13:31 -0500

    modify day 16 quote generator for bg video size and blog link function

--- day-16-quote-generator/src/components/Header.js
+++ day-16-quote-generator/src/components/Header.js
@@ -5,7 +5,10 @@ function Header() {
     <React.Fragment>
       <h2>Welcome to the React Quote Generator</h2>
       <p>
-        Built and styled by <a href='www.dev.to/jwhubert91'>James Hubert</a>
+        Built and styled by{' '}
+        <a href='https://www.dev.to/jwhubert91' target='_none'>
+          James Hubert
+        </a>
       </p>
     </React.Fragment>
   );
