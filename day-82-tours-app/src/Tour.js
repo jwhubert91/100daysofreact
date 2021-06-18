ad6c932cb92adbbe427857dcfbee0e7c6131b19b
James Hubert <jwhubert91@gmail.com>
2021-06-18 00:09:45 -0400

    Add day 82 tours app

---
+++ day-82-tours-app/src/Tour.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+const Tour = ({id,image,info,price,name,removeTour}) => {
+  const [readMore,setReadMore] = useState(false)
+
+  return <article className='single-tour'>
+    <img src={image} alt={name} />
+    <footer>
+      <div className='tour-info'>
+        <h4>{name}</h4>
+        <h4 className='tour-price'>${price}</h4>
+      </div>
+      <p>{readMore ? info : `${info.substring(0,200)}...`}</p>
+      <button onClick={() => setReadMore(!readMore)}></button>
+      <button className='delete-btn' onClick={() => removeTour(id)}>
+        Not interested
+      </button>
+    </footer>
+  </article>;
+};
+
+export default Tour;
---
+++ day-82-tours-app/src/Tours.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Tour from './Tour';
+const Tours = ({tours,removeTour}) => {
+  return <section>
+    <div>
+      <h2>Our Tours</h2>
+      <div className='underline'></div>
+    </div>
+    <div>
+      {tours.map((tour) => {
+        return <Tour key={tour.id} {...tour} removeTour={removeTour}></Tour>
+      }
+      )}
+    </div>
+  </section>;
+};
+
+export default Tours;
